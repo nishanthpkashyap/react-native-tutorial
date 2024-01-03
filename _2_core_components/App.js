951416ecadc859e4de244b73583f85810b859055
nishanth.p <nishath.p@geekyants.com>
2024-01-03 18:34:55 +0530

    text core component

--- _2_core_components/App.js
+++ _2_core_components/App.js
@@ -1,10 +1,12 @@
 import { View } from 'react-native';
 import View_1 from './components/_1_View';
+import Text_2 from './components/_2_Text';
 
 export default function App(){
   return(
-    <View style={{flex: 1}}>
-      <View_1/>
+    <View style={{flex: 1, backgroundColor: "plum", padding: 60}}>
+      {/* <View_1/> */}
+      <Text_2/>
     </View>
   );
 }
---
+++ _2_core_components/components/_2_Text.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function Text_2(){
+    return (
+        <View>
+            <Text>
+                <Text style={{color: "white"}}>Hello</Text> World
+            </Text>
+        </View>
+    )
+}
