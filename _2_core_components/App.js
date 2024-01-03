0e5dcbcf1833ef9fb4a3899ad8fe99900db703aa
nishanth.p <nishath.p@geekyants.com>
2024-01-03 18:22:23 +0530

    learnt about view core component

---
+++ _2_core_components/App.js
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import View_1 from './components/_1_View';
+
+export default function App(){
+  return(
+    <View style={{flex: 1}}>
+      <View_1/>
+    </View>
+  );
+}
---
+++ _2_core_components/components/_1_View.js
@@ -0,0 +1,10 @@
+import { View } from 'react-native'
+
+export default function View_1() {
+  return (
+    <View style={{flex: 1, backgroundColor: "plum"}}>
+        <View style={{height: 200, width: 200, backgroundColor: "lightblue"}}></View>
+        <View style={{height: 200, width: 200, backgroundColor: "lightgreen"}}></View>
+    </View>
+  )
+}
