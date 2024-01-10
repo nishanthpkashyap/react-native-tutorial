f635c3a39a131f483a79f66454cfffac97bdf635
nishanth.p <nishath.p@geekyants.com>
2024-01-10 12:00:53 +0530

    flex shrink

--- _4_layout/components/_10_Flex_Shrink.js
+++ _4_layout/components/_10_Flex_Shrink.js
@@ -5,7 +5,7 @@ export default function FlexShrink(){
     return (
         <View style={[styles.container]}>
             <Box style={{backgroundColor: "cyan", flexShrink: 1}}>Box 1 Shrink</Box>
-            <Box style={{backgroundColor: "violet", flexShrink: 1}}>Box 2 Shrink</Box>
+            <Box style={{backgroundColor: "violet", flexShrink: 2}}>Box 2 Shrink</Box>
             {/* <Box style={{backgroundColor: "yellow"}}>Box 3</Box>
             <Box style={{backgroundColor: "lightgreen"}}>Box 4</Box>
             <Box style={{backgroundColor: "lightblue"}}>Box 5</Box>
