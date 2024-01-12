8787d38f6c083091cf826470c93a6985368fa33f
nishanth.p <nishath.p@geekyants.com>
2024-01-12 18:39:01 +0530

    text input

---
+++ _8_forms/components/_1_TextInput.js
@@ -0,0 +1,29 @@
+
+import { useState } from "react";
+import { View, TextInput, StyleSheet, Text} from "react-native";
+
+export default function Textinput(){
+    const [name, setName] = useState("");
+    return (
+        <View style={[styles.container]}>
+            <TextInput style={[styles.textInput]} value={name} onChangeText={setName}/>
+            <Text style={[styles.text]}>{name}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    textInput: {
+        height: 40,
+        margin: 12,
+        padding: 10,
+        borderWidth: 1,
+    },
+    text:{
+        padding: 10,
+        fontSize: 30,
+    }
+});
