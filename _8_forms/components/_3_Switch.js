41b80dce2f4f99ac16527190906e00456b4a6fc5
nishanth.p <nishath.p@geekyants.com>
2024-01-16 12:39:25 +0530

    switch component for toggle functionality

--- _8_forms/components/_3_Switch.js
+++ _8_forms/components/_3_Switch.js
@@ -1,6 +1,6 @@
 
 import { useEffect, useState } from "react";
-import { View, TextInput, StyleSheet, Text, Switch } from "react-native";
+import { View, StyleSheet, Text, Switch } from "react-native";
 
 export default function CustomSwitch() {
 
@@ -9,16 +9,13 @@ export default function CustomSwitch() {
     useEffect(()=>{
         console.log("Dark Mode: " + isDarkMode);
     }, [isDarkMode])
-  
-    const [name, setName] = useState("");
+    
+    //<Switch /> component is used for providing toggle functionality
     return (
         <View style={[styles.container]}>
-            {/* <TextInput style={[styles.multiLineTextInput]} value={name} onChangeText={setName}
-                placeholder="Message"
-                multiline
-            /> */}
             <View style={[styles.switchContainer]}>
                 <Text style={[styles.text]}>Dark Mode</Text>
+
                 <Switch value={isDarkMode} onValueChange={setDarkMode}
                     trackColor = {{false: "red", true: "green", }}
                     thumbColor = "pink"
@@ -38,13 +35,6 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "space-between",
         paddingHorizontal: 10,
-    },
-    multiLineTextInput: {
-        margin: 12,
-        textAlignVertical: "top",
-        padding: 10,
-        borderWidth: 1,
-        minHeight: 100,
     },
     text: {
         padding: 10,
